2132963b3919b83e56d902f5e8ad64a286ac8dc7
Isaiah Chiraira <itchiraira.com>
2023-01-23 15:33:04 +0200

    Deploy to heruko

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import data from "./data.js";
+import path from "path";
 import mongoose, { connect } from "mongoose";
 import dotenv from "dotenv";
 import expressAsyncHandler from "express-async-handler";
@@ -11,7 +11,9 @@ import orderRouter from "./routes/orderRoutes.js";
 dotenv.config();
 mongoose
 
-  .connect(process.env.MONGODB_URL)
+  .connect(
+    "mongodb+srv://BroughtCargo:BroughtCargo@cluster0.lcxjdhp.mongodb.net/?retryWrites=true&w=majority"
+  )
   .then(() => {
     console.log("connected to db");
   })
@@ -27,11 +29,15 @@ app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 
-const port = process.env.PORT || 5000;
-
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
+});
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server at http://localhost:${port}`);
 });
