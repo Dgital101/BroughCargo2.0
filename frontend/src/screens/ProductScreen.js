f420ae9d391996653357d19531c2e8f00342749c
Isaiah Chiraira <itchiraira.com>
2023-03-29 13:09:16 +0200

    a

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -146,7 +146,7 @@ function ProductScreen() {
       <footer
         style={{ position: "absolute", left: "0", bottom: "0", right: "0" }}
       >
-        <div className="text-center">All rights reserved</div>
+        <div className="text-center">All rights reserved BroughtCargo</div>
       </footer>
     </div>
   );
