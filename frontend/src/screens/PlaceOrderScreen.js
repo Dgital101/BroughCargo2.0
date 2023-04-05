2d3dd84661d8f1c9180e9811d7f6775fd4162c7e
Isaiah Chiraira <itchiraira.com>
2023-04-05 14:09:16 +0200

    Fixed the Preview Order  screen

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -76,20 +76,20 @@ export default function PlaceOrderScreen() {
     }
   }, [cart, navigate]);
   return (
-    <div style={{ padding: "2rem" }}>
+    <div>
       <CheckoutSteps step1 step2 step3 step4></CheckoutSteps>
       <Helmet>
         <title>Preview Order</title>
       </Helmet>
 
-      <h1 className="my-3">Preview Order</h1>
       <Row style={{ padding: "2rem" }}>
+        <h1 className=" text-center">Preview Order</h1>
         <Col md={7}>
-          <Card className="mb-3">
+          <Card className="mb-3" style={{ backgroundColor: "#e5dbd3" }}>
             <Card.Body>
-              <Card.Title>Shipping</Card.Title>
+              <Card.Title>Shipping details</Card.Title>
               <Card.Text>
-                <strong>Name:</strong> {cart.shippingAddress.fullname} <br />
+                <strong>Name:</strong> {cart.shippingAddress.fullName} <br />
                 <strong>Address:</strong> {cart.shippingAddress.address},
                 {cart.shippingAddress.city} <br />
                 <strong>Phone Number:</strong>{" "}
@@ -99,27 +99,32 @@ export default function PlaceOrderScreen() {
             </Card.Body>
           </Card>
 
-          <Card className="mb-3 ">
+          <Card className="mb-3 " style={{ backgroundColor: "#e5dbd3" }}>
             <Card.Body>
               <Card.Title className="align-items-center">Items</Card.Title>
 
-              <ListGroup variant="flush">
+              <ListGroup variant="flush" style={{ backgroundColor: "#e5dbd3" }}>
                 {cart.cartItems.map((item) => (
-                  <ListGroup.Item key={item._id}>
-                    <Row className="align-items-center justify-content-around">
-                      <Col md={6} xs={4} fluid className=".d-flex flex-row">
-                        <img
-                          src={item.Image}
-                          alt={item.name}
-                          className="img-fluid rounded img-thumbnail"
-                        ></img>{" "}
-                        <Link to={`/product/${item.slug}`}>{item.name}</Link>
+                  <ListGroup.Item
+                    key={item._id}
+                    style={{ backgroundColor: "#e5dbd3" }}
+                  >
+                    <Row className="align-items-center justify-content-start">
+                      <Col md={6} xs={4} fluid className="d-flex flex-row">
+                        <Link to={`/product/${item.slug}`}>
+                          {" "}
+                          <img
+                            src={item.Image}
+                            alt={item.name}
+                            className="img-fluid rounded img-thumbnail"
+                          ></img>{" "}
+                        </Link>
                       </Col>
-                      <Col md={3} xs={3}>
+                      <Col md={3} xs={1}>
                         {" "}
                         <span>{item.quantity}</span>
                       </Col>
-                      <Col md={3} xs={3}>
+                      <Col md={3} xs={5}>
                         {" "}
                         R{item.price}
                       </Col>
@@ -130,7 +135,7 @@ export default function PlaceOrderScreen() {
               <Link to="/cart"> Edit</Link>
             </Card.Body>
           </Card>
-          <Card className="mb-3">
+          <Card className="mb-3" style={{ backgroundColor: "#e5dbd3" }}>
             <Card.Body>
               <Card.Title>Payment</Card.Title>
               <Card.Text>
@@ -141,24 +146,24 @@ export default function PlaceOrderScreen() {
           </Card>
         </Col>
         <Col md={4}>
-          <Card>
+          <Card style={{ backgroundColor: "#e5dbd3" }}>
             <Card.Body>
               <Card.Title>Order Summary</Card.Title>
-              <ListGroup variant="flush">
-                <ListGroup.Item>
-                  <Row>
+              <ListGroup variant="flush" style={{ backgroundColor: "#e5dbd3" }}>
+                <ListGroup.Item style={{ backgroundColor: "#e5dbd3" }}>
+                  <Row style={{ backgroundColor: "#e5dbd3" }}>
                     <Col>Items</Col>
                     <Col>R{cart.itemsPrice.toFixed(2)}</Col>
                   </Row>
                 </ListGroup.Item>
-                <ListGroup.Item>
-                  <Row>
+                <ListGroup.Item style={{ backgroundColor: "#e5dbd3" }}>
+                  <Row style={{ backgroundColor: "#e5dbd3" }}>
                     <Col>Shipping</Col>
                     <Col>R{cart.shippingPrice.toFixed(2)}</Col>
                   </Row>
                 </ListGroup.Item>
-                <ListGroup.Item>
-                  <Row>
+                <ListGroup.Item style={{ backgroundColor: "#e5dbd3" }}>
+                  <Row style={{ backgroundColor: "#e5dbd3" }}>
                     <Col>
                       <strong>Order Total</strong>
                     </Col>
@@ -167,7 +172,7 @@ export default function PlaceOrderScreen() {
                     </Col>
                   </Row>
                 </ListGroup.Item>
-                <ListGroup.Item>
+                <ListGroup.Item style={{ backgroundColor: "#e5dbd3" }}>
                   <div className="d-grid">
                     <Button
                       type="button"
@@ -191,11 +196,6 @@ export default function PlaceOrderScreen() {
           </Card>
         </Col>
       </Row>
-      <footer
-        style={{ position: "absolute", left: "0", bottom: "0", right: "0" }}
-      >
-        <div className="text-center">All rights reserved</div>
-      </footer>
     </div>
   );
 }
