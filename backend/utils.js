67c0efa846988a8eb8a219122eb780ae7550abba
Isaiah Chiraira <itchiraira.com>
2023-04-06 15:13:06 +0200

    Added dashboard screen

--- backend/utils.js
+++ backend/utils.js
@@ -1,21 +1,24 @@
 import jwt from "jsonwebtoken";
+
 export const generateToken = (user) => {
   return jwt.sign(
     {
-      id_: user.id_,
+      _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
     },
     process.env.JWT_SECRET,
-    { expiresIn: "30d" }
+    {
+      expiresIn: "30d",
+    }
   );
 };
 
 export const isAuth = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (authorization) {
-    const token = authorization.slice(7, authorization.length);
+    const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
     jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
       if (err) {
         res.status(401).send({ message: "Invalid Token" });
