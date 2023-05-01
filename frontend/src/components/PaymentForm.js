ed8e3f468c05627fd6372f83c544381a6a49f724
Isaiah Chiraira <itchiraira.com>
2023-05-01 13:25:04 +0200

    Testing payment system

--- frontend/src/components/PaymentForm.js
+++ frontend/src/components/PaymentForm.js
@@ -3,12 +3,12 @@ import { Button, Form } from "react-bootstrap";
 export default function PaymentForm({ amount }) {
   return (
     <Form
-      action="https://sandbox.payfast.co.za​/eng/process"
+      action="https://www.payfast.co.za/eng/process"
       method="POST"
       className="mt-1"
     >
-      <Form.Control type="hidden" name="merchant_id" value="10000100" />
-      <Form.Control type="hidden" name="merchant_key" value="46f0cd694581a" />
+      <Form.Control type="hidden" name="merchant_id" value="22206578" />
+      <Form.Control type="hidden" name="merchant_key" value="ogxbqwgnzukz1" />
       <Form.Control type="hidden" name="amount" value={amount} />
       <Form.Control type="hidden" name="item_name" value="Test Product" />
       <Button variant="primary" type="submit">
